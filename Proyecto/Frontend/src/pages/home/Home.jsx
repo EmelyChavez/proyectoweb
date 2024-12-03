ad88edfadbae39382d820840687e20b65a9cdf08
Jaime Jimenez <jimenez.climaco@gmail.com>
2024-12-03 14:34:45 -0600

    Add: Pets & petsCard

--- Proyecto/Frontend/src/pages/home/Home.jsx
+++ Proyecto/Frontend/src/pages/home/Home.jsx
@@ -14,6 +14,8 @@ import AppointmentAssignment from "../../components/veterinario/appointmentAssig
 import VaccineAssignment from "../../components/veterinario/vaccineAssignment/VaccineAssignment";
 import MedicalRecordList from "../../components/veterinario/medicalRecordList/MedicalRecordList";
 import MedicalRecordCard from "../../components/veterinario/medicalRecordCard/MedicalRecordCard";
+import Pets from "../../components/administrador/Pets/Pets";
+import HomeAdmin from "../../pages/aministrador/HomeAdmin"
 
 const Home = () => {
   return (
@@ -23,7 +25,7 @@ const Home = () => {
         <Route path="/informacion-mascota/:appointmentId" element={<SelectedPet />} />
         <Route path="/citas-veterinario" element={<Appointments />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<HomeClient />} />
+        <Route path="/" element={<HomeClient  />} />
         <Route path="/sobre-nosotros" element={<HomeClient />} />
         <Route path="/registrar-mascota" element={<PetRegistration />} />
         <Route path="/agendar-cita" element={<Appointment />} />
